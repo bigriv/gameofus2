03efcfa3bb0a1bc88ee77315aa99810d1a068c0d
t.yoshida <bigriv1344@gmail.com>
2023-12-02 02:06:42 +0900

    add: idのutilを作成

---
+++ src/composables/utils/id.ts
@@ -0,0 +1,24 @@
+export const generateUuid = () => {
+  return "xxxx-xxxx-xxxx-xxxx-xxxx".replace(/x/g, function () {
+    return Math.floor(Math.random() * 16).toString(16);
+  });
+};
+
+export class SequenceId {
+  private id: number;
+  readonly start: number;
+  readonly intarval: number;
+  constructor(start?: number, intarval?: number) {
+    this.id = (start ?? 0) - 1;
+    this.start = (start ?? 0) - 1;
+    this.intarval = intarval ?? 1;
+  }
+
+  generateId() {
+    this.id += this.intarval;
+    return this.id;
+  }
+  restart() {
+    this.id = this.start;
+  }
+}

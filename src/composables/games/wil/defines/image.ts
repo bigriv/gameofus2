089c54ee10aeec1833b0507ba17090ea0bbc9faf
yoshiwo13 <pandemiccer@gmail.com>
2024-01-18 00:11:59 +0900

    add: [Wil]2章使用分素材追加

--- src/composables/games/wil/defines/image.ts
+++ src/composables/games/wil/defines/image.ts
@@ -2,15 +2,27 @@ const WIL_IMAGE_ROOT_PATH = "/games/wil/images/";
 
 export const WIL_IMAGE_DEFINES: { [key: string]: string } = {
   // キャラクター（立ち絵）
-  CHARACTER_HERO: WIL_IMAGE_ROOT_PATH + "characters/standings/hero.svg",
+  CHARACTER_HERO: WIL_IMAGE_ROOT_PATH + "characters/standings/hero.png",
   CHARACTER_HOLY_KNIGHTS_LEADER:
-    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_leader.svg",
+    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_leader.png",
   CHARACTER_HOLY_KNIGHTS_SOLDIER:
-    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_soldier.svg",
+    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_soldier.png",
   CHARACTER_HOLY_KNIGHTS_MAGICIAN:
-    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_magician.svg",
+    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_knights_magician.png",
+  CHARACTER_HOLY_LAND_EMPEROR:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/holy_land_emperor.png",
+  CHARACTER_ICE_LAND_KING:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/ice_land_king.png",
+  CHARACTER_ICICLE_GURDIANS_QUEEN:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/icicle_gurdians_queen.png",
+  CHARACTER_ICICLE_GURDIANS_DEFENDER:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/icicle_gurdians_defender.png",
+  CHARACTER_ICICLE_GURDIANS_MAGICIAN:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/icicle_gurdians_magician.png",
   CHARACTER_DARK_MONSTER_SHADOW:
-    WIL_IMAGE_ROOT_PATH + "characters/standings/dark_monster_shadow.svg",
+    WIL_IMAGE_ROOT_PATH + "characters/standings/dark_monster_shadow.png",
+  CHARACTER_DARK_MONSTER_ICE_DEMON:
+    WIL_IMAGE_ROOT_PATH + "characters/standings/dark_monster_ice_demon.png",
 
   // キャラクター（ドット絵）
   // 主人公
@@ -50,6 +62,38 @@ export const WIL_IMAGE_DEFINES: { [key: string]: string } = {
   DOT_CHARACTER_HOLY_KNIGHTS_MAGICIAN3:
     WIL_IMAGE_ROOT_PATH + "characters/dots/holy_knights_magician3.png",
 
+  // 氷の守護者 王女
+  DOT_CHARACTER_ICICLE_GURDIANS_QUEEN1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_queen1.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_QUEEN2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_queen2.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_QUEEN3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_queen3.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_QUEEN4:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_queen4.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_QUEEN5:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_queen5.png",
+
+  // 氷の守護者 守護兵
+  DOT_CHARACTER_ICICLE_GURDIANS_DEFENDER1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_defender1.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_DEFENDER2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_defender2.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_DEFENDER3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_defender3.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_DEFENDER4:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_defender4.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_DEFENDER5:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_defender5.png",
+
+  // 氷の守護者 補助兵
+  DOT_CHARACTER_ICICLE_GURDIANS_MAGICIAN1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_magician1.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_MAGICIAN2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_magician2.png",
+  DOT_CHARACTER_ICICLE_GURDIANS_MAGICIAN3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/icicle_gurdians_magician3.png",
+
   // 闇の魔物 シャドウ
   DOT_CHARACTER_DARK_MONSTER_SHADOW1:
     WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_shadow1.png",
@@ -62,24 +106,66 @@ export const WIL_IMAGE_DEFINES: { [key: string]: string } = {
   DOT_CHARACTER_DARK_MONSTER_SHADOW5:
     WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_shadow5.png",
 
+  // 闇の魔物 ラビット
+  DOT_CHARACTER_DARK_MONSTER_RABBIT1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_rabbit1.png",
+  DOT_CHARACTER_DARK_MONSTER_RABBIT2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_rabbit2.png",
+  DOT_CHARACTER_DARK_MONSTER_RABBIT3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_rabbit3.png",
+  DOT_CHARACTER_DARK_MONSTER_RABBIT4:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_rabbit4.png",
+  DOT_CHARACTER_DARK_MONSTER_RABBIT5:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_rabbit5.png",
+
+  // 闇の魔物 アンデッド
+  DOT_CHARACTER_DARK_MONSTER_UNDEAD1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_undead1.png",
+  DOT_CHARACTER_DARK_MONSTER_UNDEAD2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_undead2.png",
+  DOT_CHARACTER_DARK_MONSTER_UNDEAD3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_undead3.png",
+  DOT_CHARACTER_DARK_MONSTER_UNDEAD4:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_undead4.png",
+  DOT_CHARACTER_DARK_MONSTER_UNDEAD5:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_undead5.png",
+
+  // 闇の魔物 氷の魔人
+  DOT_CHARACTER_DARK_MONSTER_ICE_DEMON1:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_ice_demon1.png",
+  DOT_CHARACTER_DARK_MONSTER_ICE_DEMON2:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_ice_demon2.png",
+  DOT_CHARACTER_DARK_MONSTER_ICE_DEMON3:
+    WIL_IMAGE_ROOT_PATH + "characters/dots/dark_monster_ice_demon3.png",
+
   // スキル
-  SKILL_SLASH: WIL_IMAGE_ROOT_PATH + "skills/slash.json",
-  SKILL_CROSS_SLASH: WIL_IMAGE_ROOT_PATH + "skills/cross_slash.json",
-  SKILL_BLOW: WIL_IMAGE_ROOT_PATH + "skills/blow.json",
   SKILL_ARROW_HORIZONTAL: WIL_IMAGE_ROOT_PATH + "skills/arrow_horizontal.json",
   SKILL_ARROW_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/arrow_vertical.json",
-  SKILL_HEAL: WIL_IMAGE_ROOT_PATH + "skills/heal.json",
-  SKILL_SHINE_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/shine_circle.json",
-  SKILL_SHINE_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/shine_vertical.json",
-  SKILL_SHINE_HORIZONTAL: WIL_IMAGE_ROOT_PATH + "skills/shine_horizontal.json",
+  SKILL_BLOW: WIL_IMAGE_ROOT_PATH + "skills/blow.json",
+  SKILL_CROSS_SLASH: WIL_IMAGE_ROOT_PATH + "skills/cross_slash.json",
+  SKILL_DARK_SLASH: WIL_IMAGE_ROOT_PATH + "skills/dark_slash.json",
   SKILL_DARK_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/dark_circle.json",
-  SKILL_DARK_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/dark_vertical.json",
   SKILL_DARK_HORIZONTAL: WIL_IMAGE_ROOT_PATH + "skills/dark_horizontal.json",
+  SKILL_DARK_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/dark_vertical.json",
+  SKILL_HEAL: WIL_IMAGE_ROOT_PATH + "skills/heal.json",
+  SKILL_ICE_HEXAGON: WIL_IMAGE_ROOT_PATH + "skills/ice_hexagon.json",
+  SKILL_ICE_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/ice_vertical.json",
+  SKILL_MUD_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/mud_circle.json",
+  SKILL_MUD_HORIZONTAL: WIL_IMAGE_ROOT_PATH + "skills/mud_horizontal.json",
+  SKILL_POISON: WIL_IMAGE_ROOT_PATH + "skills/poison.json",
+  SKILL_RAIN: WIL_IMAGE_ROOT_PATH + "skills/rain.json",
+  SKILL_SAND_SLASH: WIL_IMAGE_ROOT_PATH + "skills/sand_slash.json",
+  SKILL_SHINE_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/shine_circle.json",
+  SKILL_SHINE_HORIZONTAL: WIL_IMAGE_ROOT_PATH + "skills/shine_horizontal.json",
+  SKILL_SHINE_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/shine_vertical.json",
+  SKILL_SLASH: WIL_IMAGE_ROOT_PATH + "skills/slash.json",
+  SKILL_STONE_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/stone_circle.json",
+  SKILL_STONE_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/stone_vertical.json",
   SKILL_THUNDER_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/thunder_circle.json",
   SKILL_THUNDER_HORIZONTAL:
     WIL_IMAGE_ROOT_PATH + "skills/thunder_horizontal.json",
   SKILL_THUNDER_VERTICAL: WIL_IMAGE_ROOT_PATH + "skills/thunder_vertical.json",
-  SKILL_POISON: WIL_IMAGE_ROOT_PATH + "skills/poison.json",
+  SKILL_WATER_CIRCLE: WIL_IMAGE_ROOT_PATH + "skills/water_circle.json",
 
   // 訓練
   TRAINING_ATTACK: WIL_IMAGE_ROOT_PATH + "trainings/swords.svg",
@@ -89,12 +175,21 @@ export const WIL_IMAGE_DEFINES: { [key: string]: string } = {
   TRAINING_PHISIC: WIL_IMAGE_ROOT_PATH + "trainings/straw.svg",
 
   // 背景
+  BACKGROUND_BLACK: WIL_IMAGE_ROOT_PATH + "backgrounds/black.png",
   BACKGROUND_VILLAGE: WIL_IMAGE_ROOT_PATH + "backgrounds/village.png",
   BACKGROUND_VILLAGE_EVENING:
     WIL_IMAGE_ROOT_PATH + "backgrounds/village_evening.png",
   BACKGROUND_BATTLE_VILLAGE:
     WIL_IMAGE_ROOT_PATH + "backgrounds/battle_village.png",
   BACKGROUND_KINGDOM: WIL_IMAGE_ROOT_PATH + "backgrounds/kingdom.png",
+  BACKGROUND_TRAINING_ROOM:
+    WIL_IMAGE_ROOT_PATH + "backgrounds/training_room.png",
+  BACKGROUND_ICE_FIELD: WIL_IMAGE_ROOT_PATH + "backgrounds/ice_field.png",
+  BACKGROUND_ICE_LAND: WIL_IMAGE_ROOT_PATH + "backgrounds/ice_land.png",
   BACKGROUND_BATTLE_KINGDOM:
     WIL_IMAGE_ROOT_PATH + "backgrounds/battle_kingdom.png",
+  BACKGROUND_BATTLE_ICE_FIELD:
+    WIL_IMAGE_ROOT_PATH + "backgrounds/battle_ice_field.png",
+  BACKGROUND_BATTLE_ICE_LAND:
+    WIL_IMAGE_ROOT_PATH + "backgrounds/battle_ice_land.png",
 };
